d3310326e305fdba87b53da95be847982dbb04b7
Wilkes Joiner <wilkesjoiner@gmail.com>
2011-05-26 20:25:41 -0500

    added fetch-by-id

--- src/bankd/core.clj
+++ src/bankd/core.clj
@@ -2,28 +2,48 @@
 
 (declare *domain-repository*)
 
+(def *event-log* (atom {}))
+
+(defn uuid [instance]
+  (merge {:uid (str (java.util.UUID/randomUUID))} instance))
+
 (defn commit []
-  (doseq [event *domain-repository*]
-    (println event)))
+  (doseq [event @*domain-repository*]
+    (swap! *event-log*
+           (fn [el]
+             (let [events (get el (:aggregate-uid event) [])]
+               (assoc el (:aggregate-uid event)
+                      (conj events event))))))
+  (println @*event-log*))
 
 (defmacro with-domain-repository [& body]
-  `(binding [*domain-repository* []]
+  `(binding [*domain-repository* (atom [])]
      (let [result# ~@body]
        (commit)
        result#)))
 
-(defn execute-command [cmd params]
+(defn execute-command [cmd & args]
   (with-domain-repository
-    (cmd params)))
+    (apply cmd args)))
 
 (defn apply-event [aggregate event-handler attributes]
-  (let [event {:name (:name (meta event-handler))
-               :data attributes}
+  (let [event (uuid {:name (:name (meta event-handler))
+                    :data attributes})
         aggregate (event-handler aggregate event)]
-    (conj *domain-repository* (assoc event :aggregate-uid (:uid aggregate)))
+    (swap! *domain-repository* conj (assoc event :aggregate-uid (:uid aggregate)))
     aggregate))
 
+(defn should-exist [o]
+  (when (nil? (:uid o))
+    (throw (RuntimeException. "Object does not exist"))))
 
+(defn fetch-by-id [type-var id]
+  (reduce (fn [obj event]
+            (println event)
+            (let [f (resolve (:name event))]
+              (f obj event)))
+          ^{:type type-var} {}
+          (get @*event-log* id)))
 
 (defrecord Client [])
 
@@ -34,11 +54,24 @@
            :account-uids []
            :card-numbers [])))
 
-(defn make-client [attributes]
-  (apply-event (Client.) client-created attributes))
+(defn do-create-client []
+  (apply-event (Client.) client-created nil))
 
-(defn create-client-cmd [params]
-  (make-client params))
+(defn create-client-cmd []
+  (do-create-client))
 
 (defn create-client [params]
-  (execute-command create-client-cmd params))
+  (execute-command create-client-cmd))
+
+(defn name-changed [this event]
+  (assoc this :name (-> event :data :name)))
+
+(defn change-name [client name]
+  (should-exist client)
+  (apply-event client name-changed {:client-uid (:uid client)
+                                    :name name})  )
+(defn name-client-cmd [id name]
+  (change-name (fetch-by-id Client id) name))
+
+(defn name-client [params]
+  (execute-command name-client-cmd (:id params) (:name params)))
