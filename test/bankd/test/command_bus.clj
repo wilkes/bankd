3b5df7954e9292f8dad7dee4a81fde4d3fe567c1
Wilkes Joiner <wilkes@aspic-dev.(none)>
2011-06-03 18:05:36 -0500

    fixed broken command-bus test

--- test/bankd/test/command_bus.clj
+++ test/bankd/test/command_bus.clj
@@ -2,11 +2,6 @@
   (:use bankd.command-bus
         midje.sweet))
 
-(unfinished call-me)
-
-;.;. FAIL at (NO_SOURCE_FILE:1)
-;.;. You claimed the following was needed, but it was never used:
-;.;.     (call-me .result.)
 (facts "execute-command"
   (fact "executes the command and returns the result"
     (execute-command .cmd. .arg.) => .result.
@@ -16,5 +11,4 @@
     (execute-command .cmd. .arg.) => .result.
     (provided
       (.cmd. .arg.) => .result.
-      (commit-repository! .result.) => .committed.
-      (call-me .result.) => .what-evs.)))
+      (commit-repository!) => .committed.)))
